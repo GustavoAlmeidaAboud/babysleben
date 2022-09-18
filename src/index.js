2c13a17c07a57650bdd8ace0cb7904ba9591b4ac
Gustavo Almeida Aboud <almeida.gustavo@hotmail.fr>
2022-09-19 00:28:34 +0100

    everything now

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM  from "react-dom";
 import App from "./App";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
